873386f908b044aa2866f6aff774237964320e17
Amadewe <dewevre.amandine@gmail.com>
2019-01-23 16:41:33 +0100

    Modification de l'exercice 7 avec regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,29 @@
     // Ajouter au tout 1766
 
 function myFunction() {
+  var regex = new RegExp (/^[0-9]{2}$/);
+  // 2 entre {} pour dire qu'on veut juste une poiture à deux chiffres idem pour 4
+  var regex2 = new RegExp (/^[0-9]{4}$/);
+
 var shoeSize = document.getElementById("shoeSize").value;
 var yearOfBirth = document.getElementById("yearOfBirth").value;
-if(isNaN(shoeSize)==false && isNaN(yearOfBirth)==false){
+if(regex.test(shoeSize) & regex2.test(yearOfBirth)){
 var result = (((shoeSize*2)+5)*50)-(yearOfBirth)+1766;
 alert(result);
 } else {
 alert ('merci de remplir des chiffres');
 }
 }
+
+var regex = new RegExp(/^[1-9]{2}&/);
+    var regex2 = new RegExp(/^[1-9]{4}&/);
+    var shoeSize = document.getElementById('shoeSize').value;
+    var yearOfBirth = document.getElementById('yearOfBirth').value;
+
+    var result = ((((shoeSize * 2)+5)*50) - yearOfBirth)+1766 ;
+
+    if (regex.test(shoeSize) & regex2.test(yearOfBirth)){
+      alert(result);
+    }
+    else{
+      alert('entrer une année et une pointure raisonnable différent de 0 et pas de lettres svp.')
